5f535e9e047ba944f8f3320165d293a5c88ae98b
hertogp <git.hertogp@gmail.com>
2021-02-27 15:23:07 +0100

    initial setup

---
+++ lx/lib/lx.ex
@@ -0,0 +1,18 @@
+defmodule Lx do
+  @moduledoc """
+  Documentation for `Lx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Lx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
